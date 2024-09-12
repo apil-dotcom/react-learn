1ad04314f815ceee514b27f10e6e3feb72bb6fce
apil-dotcom <apilneupane19@gmail.com>
2024-09-12 11:50:52 +0545

    create banaiyo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react' 
+import {Link} from "react-router-dom"
 
 const Navbar = () => {
   return (
@@ -28,7 +29,7 @@ const Navbar = () => {
         <div className="hidden sm:ml-6 sm:block">
           <div className="flex space-x-4">
           
-            <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">+add</a>
+            <Link to='/create' className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">+add</Link>
            
           </div>
         </div>
@@ -41,7 +42,7 @@ const Navbar = () => {
   <div className="sm:hidden" id="mobile-menu">
     <div className="space-y-1 px-2 pb-3 pt-2">
      
-      <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">+add</a>
+      <Link to="/create" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">+add</Link>
       
     </div>
   </div>
